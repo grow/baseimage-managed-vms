7a004d0bfc94a34fda9d48ac6cb930a730af7aa3
Mike Fotinakis <mike@fotinakis.com>
2015-08-11 18:01:46 -0700

    Fix tagging problems.

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -e
 
 # Also needs to be updated in Dockerfile when changed.
 GROW_VERSION=0.0.53
+IMAGE="grow/baseimage-managed-vms"
 
-docker build -t grow/baseimage-managed-vms:$GROW_VERSION -t grow/baseimage-managed-vms:latest .
+docker build -t $IMAGE:$GROW_VERSION .
+docker tag -f $IMAGE:$GROW_VERSION $IMAGE:latest
